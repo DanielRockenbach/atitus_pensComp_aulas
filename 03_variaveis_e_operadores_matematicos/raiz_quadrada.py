1a5e2ad970b84b450bd1d83f55ceed08406f7727
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-03-19 22:08:28 -0300

    alteracao

--- 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
+++ 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
@@ -1,8 +1,13 @@
 def raiz_quadrada(valor):
-    # Preencher
+    return valor ** (1 / 2)
     pass
 
+def test():
+    assert raiz_quadrada(9) == 3
+    assert raiz_quadrada(16) == 4
+    assert raiz_quadrada(25) == 5
 
-assert raiz_quadrada(9) == 3
-assert raiz_quadrada(16) == 4
-assert raiz_quadrada(25) == 5
+print("Terminado com sucesso")
+print(raiz_quadrada(9) == 3)
+print(raiz_quadrada(16) == 4)
+print(raiz_quadrada(25) == 5)
