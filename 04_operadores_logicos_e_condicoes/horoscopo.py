4bb942d7128e83bd16e9f6bf644d2471068bd92e
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-03-25 11:59:14 -0300

    salvo3

--- 04_operadores_logicos_e_condicoes/horoscopo.py
+++ 04_operadores_logicos_e_condicoes/horoscopo.py
@@ -11,22 +11,22 @@ def horoscopo(mes):
  
 
 def test():
-    assert horoscopo(1) == "Python"
-    assert horoscopo(2) == "Python"
+     assert horoscopo(1) == "Python"
+     assert horoscopo(2) == "Python"
 
-    assert horoscopo(4) == "Java"
-    assert horoscopo(6) == "Java"
+     assert horoscopo(4) == "Java"
+     assert horoscopo(6) == "Java"
 
-    assert horoscopo(7) == "PHP"
-    assert horoscopo(9) == "PHP"
+     assert horoscopo(7) == "PHP"
+     assert horoscopo(9) == "PHP"
 
-    assert horoscopo(10) == "TypeScript"
-    assert horoscopo(12) == "TypeScript"
+     assert horoscopo(10) == "TypeScript"
+     assert horoscopo(12) == "TypeScript"
 
-    assert horoscopo(-1) is None
-    assert horoscopo(0) is None
-    assert horoscopo(13) is None
-
+     assert horoscopo(-1) is None
+     assert horoscopo(0) is None
+     assert horoscopo(13) is None
+    
 print(horoscopo(10))
 print(horoscopo(-1))
 print(horoscopo(0))
--- 04_operadores_logicos_e_condicoes/verifica_positivo.py
+++ 04_operadores_logicos_e_condicoes/verifica_positivo.py
@@ -10,19 +10,19 @@ def eh_negativo(numero):
         return True
 
 def test(): 
-    assert eh_positivo(1)
-    assert eh_positivo(2)
-    assert eh_positivo(10)
-    assert not eh_positivo(0)
-    assert not eh_positivo(-1)
-    assert not eh_positivo(-10)
-
-    assert eh_negativo(-1)
-    assert eh_negativo(-2)
-    assert eh_negativo(-10)
-    assert not eh_negativo(0)
-    assert not eh_negativo(1)
-    assert not eh_negativo(10)
+     assert eh_positivo(1)
+     assert eh_positivo(2)
+     assert eh_positivo(10)
+     assert not eh_positivo(0)
+     assert not eh_positivo(-1)
+     assert not eh_positivo(-10)
+ 
+     assert eh_negativo(-1)
+     assert eh_negativo(-2)
+     assert eh_negativo(-10)
+     assert not eh_negativo(0)
+     assert not eh_negativo(1)
+     assert not eh_negativo(10)
 
 print(eh_negativo(-10))
 print(eh_positivo(6))
