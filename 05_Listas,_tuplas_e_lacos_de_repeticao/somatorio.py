a9c9648a2ce303971b771e06b13b58f25879916a
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-03-28 16:29:56 -0300

    salvo3

--- 05_Listas,_tuplas_e_lacos_de_repeticao/somatorio.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/somatorio.py
@@ -1,24 +1,22 @@
 def somatorio(numero):
-		if numero < 0:
-	   		return None
-		resultado = 0
-		for numero in range(numero):
-        		print(numero, [numero + numero +! for numero in range(numero)])
-			resultado = 		
-return resultado 
-	
+    resultado = 0
+    for n in range(numero + 1):
+        resultado += n
+        print(n, resultado)
+    return resultado
 
 def test():
     assert somatorio(-1) is None
-    assert somatorio(0) == (0)
-    assert somatorio(1) == (1)
-    assert somatorio(2) == (3)
-    assert somatorio(3) == (6)
-    assert somatorio(4) == (10)
-    assert somatorio(5) == (15)
-    assert somatorio(6) == (21)
-    assert somatorio(7) == (28)
-    assert somatorio(8) == (36)
-    assert somatorio(9) == (45)
+    assert somatorio(0) == 0
+    assert somatorio(1) == 1
+    assert somatorio(2) == 3
+    assert somatorio(3) == 6
+    assert somatorio(4) == 10
+    assert somatorio(5) == 15
+    assert somatorio(6) == 21
+    assert somatorio(7) == 28
+    assert somatorio(8) == 36
+    assert somatorio(9) == 45
 
-print(somatorio(20))
+print(somatorio(100))
+
