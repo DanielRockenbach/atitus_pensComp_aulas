98b03048486a31311912478bca5f050c662e07b3
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-04-21 08:11:48 -0300

    main

--- 04_operadores_logicos_e_condicoes/horoscopo.py
+++ 04_operadores_logicos_e_condicoes/horoscopo.py
@@ -1,33 +1,33 @@
-#def horoscopo(mes):
-#	if mes <= 0 or mes > 12:
- #		return None
-#	if mes <= 3:
- #         return "Python"
-#	if mes <= 6: 
- #         return "Java"
-#	if mes <= 9:
- #		return "PHP"
-#	else:
- #         return "TypeScript"
-  #   return
+def horoscopo(mes):
+	if mes <= 0 or mes > 12:
+ 		return None
+	if mes <= 3:
+          return "Python"
+	if mes <= 6: 
+          return "Java"
+	if mes <= 9:
+ 		return "PHP"
+	else:
+          return "TypeScript"
+     return
 
-#def test():
-#     assert horoscopo(1) == "Python"
-#     assert horoscopo(2) == "Python"
+def test():
+     assert horoscopo(1) == "Python"
+     assert horoscopo(2) == "Python"
 
-#     assert horoscopo(4) == "Java"
-#     assert horoscopo(6) == "Java"
+     assert horoscopo(4) == "Java"
+     assert horoscopo(6) == "Java"
 
-#     assert horoscopo(7) == "PHP"
-#     assert horoscopo(9) == "PHP"
+     assert horoscopo(7) == "PHP"
+     assert horoscopo(9) == "PHP"
 
-#     assert horoscopo(10) == "TypeScript"
-#     assert horoscopo(12) == "TypeScript"
+     assert horoscopo(10) == "TypeScript"
+     assert horoscopo(12) == "TypeScript"
 
-#     assert horoscopo(-1) is None
-#     assert horoscopo(0) is None
-#     assert horoscopo(13) is None
+     assert horoscopo(-1) is None
+     assert horoscopo(0) is None
+     assert horoscopo(13) is None
     
-#print(horoscopo(10))
-#print(horoscopo(-1))
-#print(horoscopo(0))
+print(horoscopo(10))
+print(horoscopo(-1))
+print(horoscopo(0))
