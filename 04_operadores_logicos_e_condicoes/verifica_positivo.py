a025da410ba99e6c5b3ddf0864a3403b269cb6bc
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-04-21 08:10:24 -0300

    main

--- 04_operadores_logicos_e_condicoes/verifica_positivo.py
+++ 04_operadores_logicos_e_condicoes/verifica_positivo.py
@@ -1,28 +1,23 @@
 def eh_positivo(numero):
-    if numero > 0:
-        return True 
-    else:
-        return False
-
+    return numero > 0  
 
 def eh_negativo(numero):
-    if not (numero > 0):
-        return True
+    return numero < 0  
 
 def test(): 
      assert eh_positivo(1)
      assert eh_positivo(2)
      assert eh_positivo(10)
-     assert not eh_positivo(0)
+     assert not eh_positivo(0)  
      assert not eh_positivo(-1)
      assert not eh_positivo(-10)
  
      assert eh_negativo(-1)
      assert eh_negativo(-2)
      assert eh_negativo(-10)
-     assert not eh_negativo(0)
+     assert not eh_negativo(0)  
      assert not eh_negativo(1)
      assert not eh_negativo(10)
 
-print(eh_negativo(-10))
-print(eh_positivo(6))
+print(eh_negativo(-10))  
+print(eh_positivo(6)) 
