cd35cb0c16e14e5dbc5ad1421a0c463b3abe2623
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-05-20 16:29:05 -0300

    main

--- 07_Exercicios/extra_04.py
+++ 07_Exercicios/extra_04.py
@@ -9,23 +9,21 @@ def calcula_classe_social(salarios, salario_minimo):
 
     if salarios_minimos_per_capita > 15:
         return "A"
-    elif 5 <= salarios_minimos_per_capita <= 15:
+    elif 5 < salarios_minimos_per_capita <= 15:  
         return "B"
-    elif 3 <= salarios_minimos_per_capita < 5:
+    elif 2 < salarios_minimos_per_capita <= 5:   
         return "C"
-    elif 1 <= salarios_minimos_per_capita < 3:
+    elif 1 < salarios_minimos_per_capita <= 2:   
         return "D"
     else:
         return "E"
 
 def test():
     assert calcula_classe_social([], 1000) is None
-    assert calcula_classe_social([1000], 1000) == "E"
-    assert calcula_classe_social([500], 1000) == "E"
-    assert calcula_classe_social([1000, 0], 900) == "E"
-    assert calcula_classe_social([1000], 900) == "D"
-    assert calcula_classe_social([10000, 15000], 1000) == "B"
-    assert calcula_classe_social([20000, 25000], 1000) == "A"
-    assert calcula_classe_social([20000, 0, 0, 0, 0], 1000) == "C"
-    print("Todos os testes passaram!")
-
+    assert calcula_classe_social([1000], 1000) == "E"    
+    assert calcula_classe_social([500], 1000) == "E"     
+    assert calcula_classe_social([1000, 0], 900) == "E"  
+    assert calcula_classe_social([1000], 900) == "E"    
+    assert calcula_classe_social([10000, 15000], 1000) == "B"  
+    assert calcula_classe_social([20000, 25000], 1000) == "A"  
+    assert calcula_classe_social([20000, 0, 0, 0, 0], 1000) == "C"  
