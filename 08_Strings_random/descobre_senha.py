75fe383d7efe801212d0cd8910691e6a6a061260
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-05-20 16:38:07 -0300

    main

--- 08_Strings_random/descobre_senha.py
+++ 08_Strings_random/descobre_senha.py
@@ -18,7 +18,7 @@ def advinha_senha(numero_sorteado, palpites):
 def test():
     numero = 7
     palpites = [3, 8, 7]
-    resultado = advinha_senha(numero_sorteado, palpites)
+    resultado = advinha_senha(numero, palpites)
 
     assert resultado[0] == "O número sorteado é MAIOR do que o seu palpite."
     assert resultado[1] == "O número sorteado é MENOR do que o seu palpite."
