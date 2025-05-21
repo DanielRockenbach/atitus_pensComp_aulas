4170a02c244b68a8f41f269b2439c67bdf6828fe
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-05-21 11:19:49 -0300

    main

--- 11_Funcoes_e_parametros_avancados/lista_primos.py
+++ 11_Funcoes_e_parametros_avancados/lista_primos.py
@@ -19,6 +19,6 @@ def test():
     assert lista_primos(10) == [2, 3, 5, 7]
     assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
     assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-    print("All tests passed!")
+    
 
 print(lista_primos(100))
