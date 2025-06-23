ac17d47a3d879e3c2e306056ddc8712ecf4f1102
DanielRockenbach <danielrockenbach2005@gmail.com>
2025-06-22 21:49:37 -0300

    main

--- 12_Funcoes_e_imports/juros_compostos.py
+++ 12_Funcoes_e_imports/juros_compostos.py
@@ -1,24 +1,18 @@
-def calcular_juros_compostos(valor: int, taxa:int, tempo: int) -> int|None :
-    if valor < 0:
-        return None
-    if tempo < 0:
-        return None
-    return valor * ((1 + taxa)** tempo)
-        
-        
-
+def calcular_juros_compostos(principal, taxa, tempo):
+    return principal * (1 + taxa) ** tempo
 
 
 def calcular_juros_compostos_recursivo(principal, taxa, tempo):
     if tempo == 0:
-        return principaç
-    return calcular_juros_compostos_recursivo(principal, taxa, tempo - 1)* (1 + taxa)
+        return principal
+    return calcular_juros_compostos_recursivo(principal, taxa, tempo - 1) * (1 + taxa) 
 
-def test():
+def test_calcular_juros_compostos():
     assert calcular_juros_compostos(1000, 0.05, 5) == 1276.2815625000003
-    assert calcular_juros_compostos(1000, 0.05, 10) == 1628.894626777442
+    assert calcular_juros_compostos(1000, 0.05, 10) == 1628.894626777442 
     assert calcular_juros_compostos(1000, 0.05, 0) == 1000
 
+def test_calcular_juros_compostos_recursivo():
     assert calcular_juros_compostos_recursivo(1000, 0.05, 5) == 1276.2815625000003
-    assert calcular_juros_compostos_recursivo(1000, 0.05, 10) == 1628.894626777442
-    assert calcular_juros_compostos_recursivo(1000, 0.05, 0) == 1000
+    assert calcular_juros_compostos_recursivo(1000, 0.05, 10) == 1628.8946267774422
+    assert calcular_juros_compostos_recursivo(1000, 0.05, 0) == 1000  
